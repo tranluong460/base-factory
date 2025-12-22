acb5ba53c128e151b117aa620a249cb767f0721f
Alone <tranluong460@gmail.com>
2025-12-23 01:26:30 +0700

    refactor(core): remove Base class and update exports in utils index

--- src/utils/base.ts
+++
@@ -1,9 +0,0 @@
-import { CoreLogger } from '@vitechgroup/mkt-elec-core'
-
-export class Base {
-  protected logger: CoreLogger
-
-  constructor() {
-    this.logger = CoreLogger.getInstance()
-  }
-}
